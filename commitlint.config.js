33f10bff74b10caac737b3be86bdd11db0e693ba
NeeKin <neekin@192.168.2.65>
2023-02-23 13:14:21 +0800

    style: add config to commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'pref', 'revert', 'build'],
+    ],
+    'subject-case': [0],
+  },
+}
